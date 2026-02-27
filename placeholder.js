4547964176382dfb54cdfff67bf268b656885eb8
Chacy5 <112689000+Chacy5@users.noreply.github.com>
2026-02-27 21:55:15 +0400

    Refactor IIFE syntax in placeholder.js

--- placeholder.js
+++ placeholder.js
@@ -1,5 +1,4 @@
 (function () {
-  // Просто чтобы запоминался город при переходах
   const title = document.title || "";
   if (title.includes("Минск")) localStorage.setItem("banka_city", "minsk");
   if (title.includes("Брест")) localStorage.setItem("banka_city", "brest");
